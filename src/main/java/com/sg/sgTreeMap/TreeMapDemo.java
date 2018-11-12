81fff84b76e9db10d1c176a24e65b79189f3070a
shaogangzi <503495237@qq.com>
2018-11-12 22:45:56 +0800

    treeMap代码提交

---
+++ src/main/java/com/sg/sgTreeMap/TreeMapDemo.java
@@ -0,0 +1,19 @@
+package com.sg.sgTreeMap;
+
+
+import java.util.TreeMap;
+
+/**
+ * treeMap学习
+ */
+public class TreeMapDemo {
+    public static void main(String[] args) {
+        TreeMap<String,String> tree =  new TreeMap<String, String>();
+        tree.put("123","123");
+        tree.put("789","123");
+        tree.put("456","123");
+        tree.put("456","123");
+
+        System.out.println(tree);
+    }
+}
